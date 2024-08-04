6ecaf216bbea7aaf4ad358d8252e1186108c2e56
skettzu <99dwong@gmail.com>
2024-08-04 15:57:47 -0400

    Test MPU6050

--- RPi_GUI.py
+++ RPi_GUI.py
@@ -4,9 +4,9 @@ from smbus2 import SMBus					#import SMBus module of I2C
 from time import sleep          #import
 '''
 Author: Derek Huang
-Lab 12: RPi GUI
+Lab 14: Project
 ELEC5650-01A
-07/26/2024
+08/07/2024
 '''
 
 class MPU6050():
@@ -55,7 +55,7 @@ class MPU6050():
                     value = value - 65536
             return value
 
-    def read_all_data():
+    def read_all_data(delay):
         #Read Accelerometer raw value
         acc_x = read_raw_data(ACCEL_XOUT_H)
         acc_y = read_raw_data(ACCEL_YOUT_H)
@@ -76,33 +76,36 @@ class MPU6050():
         Gz = gyro_z/131.0
 
         print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
-        sleep(1)
+        sleep(delay)
+        data = [Ax, Ay, Az, Gx, Gy, Gz]
+        return data
 
-def cbButn_StoreSheets():
-    urlFull = urlBase + "?sliderValue=" + str(txtTale.size)
-    webResponse = urllib.request.urlopen(urlFull)
-    print(webResponse.getcode())
-    print(urlFull)
+def cbButn_StartMPU6050():
+    device = MPU6050()
+    device.MPU_Init()
+    sleep(0.1)
+    while True:
+        data = device.read_all_data_and_store(initDelay)
+        urlFull = urlBase + "?AccelX=" + str(data[0]) + "&AccelY=" + str(data[1]) + "&AccelZ=" + str(data[2]) + "&GyroX=" + str(data[3]) + "&GyroY=" + str(data[4]) + "&GyroZ=" + str(data[5])
+        webResponse = urllib.request.urlopen(urlFull)
+        txtTime.value = str(webResponse)
+        #print(urlFull)
 
-def cbButn_AddWord():
-    txtTale.value += " " + user_story.value
-    user_story.clear()
 
-def cbSldr_FontSize(slider_value):
-    txtTale.size = slider_value
+def cbSldr_DelayTime(slider_value):
+    global initDelay
+    initDelay = slider_value
     font_size.value = f"Font Size: {slider_value}"
 
 if __name__ == '__main__':
     global urlBase
-    global initFontSize
+    global initDelay
     urlBase = "https://script.google.com/macros/s/AKfycbwzKASayNBtM3Av7-rBt5BTLKPqlJEincxLbQk1kvg/dev"
-    initFontSize = 12
-    app = App(title="Tales of Old by Derek")
-    txtTale = Text(app, text="Once upon a time", size=initFontSize, font="Times New Roman", color="red")
-    user_story = TextBox(app, width=30)
-    add_story = PushButton(app, command=cbButn_AddWord, text="Add to Story")
-    text_size = Slider(app, command=cbSldr_FontSize, start=4, end=40)
-    font_size = Text(app, text=f"Font Size: {initFontSize}")
-    store_size = PushButton(app, command=cbButn_StoreSheets, text="Store to Google Sheets")
+    initDelay = 1
+    app = App(title="MPU6050 Monitoring GUI")
+    txtTime = Text(app, text="Please Select the Time Interval To Store Data (seconds):", size=10, font="Times New Roman", color="red")
+    delay_size = Slider(app, command=cbSldr_DelayTime, start=1, end=30)
+    font_size = Text(app, text=f"Delay Time: {initDelay}")
+    store_size = PushButton(app, command=cbButn_StartMPU6050, text="Start MPU6050 Data Collection")
     app.display()
             
